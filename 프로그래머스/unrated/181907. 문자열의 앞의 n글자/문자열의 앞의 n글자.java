093349583521eda6094341f216101e28586db0a2
JunSu <skyzephyr333@gmail.com>
2024-01-06 00:32:28 +0900

    [level unrated] Title: 문자열의 앞의 n글자, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int n) {
+        // substring 소문자임
+        String answer = my_string.substring(0,n);
+        
+        return answer;
+    }
+}
