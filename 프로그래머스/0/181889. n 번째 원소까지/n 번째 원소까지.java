8451fd45f8b06319d0007c07abe36ac64f9e4329
JunSu <skyzephyr333@gmail.com>
2024-02-02 02:32:19 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        // n-1
+        
+        return Arrays.copyOfRange(num_list, 0, n);
+    }
+}
